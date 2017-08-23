a8ccbc56748d1a1fa1eb280fe4cc70811003fdb6
David Williams <thedavesignal@gmail.com>
2017-08-23 16:32:55 -0700

    Implements Binary Search Tree to objective 4, with commands to build a tree from an ordered list or a shuffled list.

--- binary-tree.rb
+++ binary-tree.rb
@@ -16,33 +16,35 @@ class BinaryTree
     @root = nil
   end
 
-  def add_leaf(leaf, current_node=@root, last_route=nil)
-    leaf = Node.new(leaf) unless leaf.is_a? Node
+  def build_tree(seed_array)
+    seed_array.each {|data| add_leaf(Node.new(data))}
+  end
 
-    if current_node == nil
+  def shuffle_then_build(seed_array)
+    shuffled_seeds = seed_array.shuffle
+    build_tree(shuffled_seeds)
+  end
+
+  def add_leaf(leaf, current_node=@root, last_route=nil)
+    if current_node.nil?
       current_node = leaf
+      current_node.parent.send(last_route, current_node) unless current_node.parent.nil?
       @root = current_node if @root == nil
-      case last_route
-        when :left then current_node.parent.left = current_node
-        when :right then current_node.parent.right = current_node
-      end
     else
       leaf.parent = current_node
       if leaf.value < current_node.value
-        add_leaf(leaf, current_node.left, :left)
+        add_leaf(leaf, current_node.left, :left=)
       else
-        add_leaf(leaf, current_node.right, :right)
+        add_leaf(leaf, current_node.right, :right=)
       end
     end
   end
-  
 end
 
 
 array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
 money_tree = BinaryTree.new
+money_tree.shuffle_then_build(array)
 
-array.each {|data| money_tree.add_leaf(data)}
-
-puts money_tree.root.right.left
+puts money_tree.root.value
